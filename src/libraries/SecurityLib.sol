9e69b117cdbe5d3889d61aaf90ae09d4e52b8e6e
Luthfi <luthfihadi543@gmail.com>
2025-05-12 14:39:46 +0700

    fix securitylib

--- src/libraries/SecurityLib.sol
+++ src/libraries/SecurityLib.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 
 library SecurityLib {
-    using ECDSA for bytes32;
-
     function recoverSigner(bytes32 messageHash, bytes memory signature) internal pure returns (address) {
-        return messageHash.toEthSignedMessageHash().recover(signature);
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(messageHash);
+        return ECDSA.recover(ethSignedMessageHash, signature);
     }
     
     function validateChallenge(bytes32 challenge, uint256 timestamp, uint256 validityWindow) internal view returns (bool) {
